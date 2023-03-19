16b945687ecb1d85d1106d9aaedbdd4990c3bf7d
moazzamsadiq <moazzam@syntaxtechs.com>
2023-03-19 14:39:24 -0400

    1

---
+++ src/test/java/class01/testNGannotations.java
@@ -0,0 +1,36 @@
+package class01;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testNGannotations {
+
+    @BeforeMethod
+    public  void beforeMethods(){
+        System.out.println("i am before Method");
+    }
+
+    @AfterMethod
+    public  void afterMethod(){
+        System.out.println("i am after method");
+    }
+
+    @Test
+    public void aFirstTestCase(){
+        System.out.println("i am the first test case");
+    }
+
+    @Test
+    public  void bSecondTestcase(){
+        System.out.println("i am the second test case");
+    }
+
+    @Test
+    public  void cThirdTestcase(){
+        System.out.println("i am thirdTest case");
+    }
+}
+
+
+//11:52
